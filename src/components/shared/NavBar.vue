0b091ff06b9d21a63322bf2f56ca4854773b57ec
David Glassman <davidglassman@fastmail.com>
2025-12-14 12:46:13 -0500

    Add ThemeSelector and initial NavBar

---
+++ src/components/shared/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <ThemeSelector />
+</template>
+
+<script setup lang="ts">
+import ThemeSelector from '@/components/shared/ThemeSelector.vue';
+
+export interface Link {
+  href: string;
+  title: string;
+}
+
+const props = defineProps<{
+  links: Link[];
+}>();
+</script>
+
+<style scoped></style>
---
+++ src/components/shared/ThemeSelector.vue
@@ -0,0 +1,40 @@
+<template>
+  <Button
+    @click="toggleTheme"
+    aria-label="Toggle theme"
+    variant="ghost"
+    size="icon"
+    class="-ml-3 bg-transparent hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 dark:hover:bg-transparent">
+    <Moon class="hidden size-5 text-gray-300 dark:inline" />
+    <Sun class="size-5 text-gray-500 dark:hidden" />
+  </Button>
+</template>
+
+<script setup lang="ts">
+import { Button } from '@/components/ui/button';
+import { Sun, Moon } from 'lucide-vue-next';
+import { onMounted } from 'vue';
+
+const toggleTheme = () => {
+  if (localStorage.theme === 'dark') {
+    localStorage.theme = 'light';
+    document.documentElement.classList.remove('dark');
+  } else {
+    localStorage.theme = 'dark';
+    document.documentElement.classList.add('dark');
+  }
+};
+
+onMounted(() => {
+  if (
+    localStorage.theme === 'dark' ||
+    (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
+  ) {
+    document.documentElement.classList.add('dark');
+  } else {
+    document.documentElement.classList.remove('dark');
+  }
+});
+</script>
+
+<style scoped></style>
